16d7c3d23b808e3760a68b776e96eed0a48b0790
Dunsin <jesudunsinfaiye@gmail.com>
2024-03-03 10:21:18 +0100

    chore: add proper metro settings

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-	transformer: {
-		getTransformOptions: async () => ({
-			transform: {
-				experimentalImportSupport: false,
-				inlineRequires: true,
-			},
-		}),
-	},
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+
+// const config = {
+// 	...,
+// 	transformer: {
+// 		getTransformOptions: async () => ({
+// 			transform: {
+// 				experimentalImportSupport: false,
+// 				inlineRequires: true,
+// 			},
+// 		}),
+// 	},
+// };
+
+module.exports = getDefaultConfig(__dirname);
